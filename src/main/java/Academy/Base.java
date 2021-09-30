f58749ecfa70f0ad4d4755ad397aef8ad7cd9f90
bojan-wik <bojan.wik@protonmail.com>
2021-09-30 15:26:07 +0200

    start repo

---
+++ src/main/java/Academy/Base.java
@@ -0,0 +1,16 @@
+package Academy;
+
+/**
+ * Jest to klasa bazowa dla mojego frameworka, która będzie uruchamiana za każdym razem, kiedy framework będzie odpalany.
+ */
+
+public class Base {
+
+    /**
+     * Tworzę metodę, która będzie odpowiedzialna za inicjalizowanie drivera. Wszystkie stworzone później test casy będą korzystały z tej właśnie metody.
+     * Dane dot. poszczególnych przeglądarek nie będą hardkodowane, tylko zaciągana z zewn. pliku (patrz 'Chapter19_GlobalEnvVariables')
+     */
+    public void initializeDriver() {
+
+    }
+}
