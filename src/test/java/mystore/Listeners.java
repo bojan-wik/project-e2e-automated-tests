fe79b96c86522d85e95e3ed4cf443e5fe852bc2c
bojan-wik <bojan.wik@protonmail.com>
2021-11-05 14:44:16 +0100

    adding 'Listeners.java'

---
+++ src/test/java/mystore/Listeners.java
@@ -0,0 +1,12 @@
+package mystore;
+
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class Listeners implements ITestListener {
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        System.out.println("test failed -> make screenshot");
+    }
+}
