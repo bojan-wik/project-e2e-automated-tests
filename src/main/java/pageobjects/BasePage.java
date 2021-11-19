68d91c9add09a81ad1df0cdec4dfd13159b6e194
bojan-wik <bojan.wik@protonmail.com>
2021-11-19 13:39:59 +0100

    SearchProduct.testCase1 broken down into smaller test cases

--- src/main/java/pageobjects/BasePage.java
+++ src/main/java/pageobjects/BasePage.java
@@ -34,8 +34,8 @@ public class BasePage {
     private By womenLinkLocator = By.xpath("//a[@title='Women']");
     private By tshirtsLinkLocator = By.xpath("//a[@title='T-shirts']");
     //private By productsListLocatorHomepage = By.xpath();
-    private By productsListLocatorNotHomepage = By.xpath("//*[@id='center_column']/ul");
-    private By firstProductLocatorNotHomepage = By.xpath("//h5[@itemprop='name'][1]");
+    private By productsListLocator = By.xpath("//*[@id='center_column']/ul");
+    private By firstProductLocator = By.xpath("//h5[@itemprop='name'][1]");
     private By newsletterEmailFieldLocator = By.id("newsletter-input");
     private By newsletterSubmitButtonLocator = By.xpath("//button[@name='submitNewsletter']");
     private By alertFailLocator = By.className("alert-danger");
@@ -49,8 +49,8 @@ public class BasePage {
     public WebElement getSearchButton() { return driver.findElement(searchButtonLocator); }
     public WebElement getWomenLink() { return driver.findElement(womenLinkLocator); }
     public WebElement getTshirtsLink() { return driver.findElement(tshirtsLinkLocator); }
-    public WebElement getProductsListNotHomepage() { return driver.findElement(productsListLocatorNotHomepage); }
-    public WebElement getFirstProductNotHomepage() { return driver.findElement(firstProductLocatorNotHomepage); }
+    public WebElement getProductsList() { return driver.findElement(productsListLocator); }
+    public WebElement getFirstProduct() { return driver.findElement(firstProductLocator); }
     /**
      * Poprzednia metoda (getLoginButton()) zwracała tylko WebElement login buttona. Potem w testcase wykonywałem na tym webelemencie click
      * i zawsze po tym następowało przejście do strony logowania i musiałem tworzyć obiekt klasy LoginPageObjects.
--- src/test/java/tests/SearchProduct.java
+++ src/test/java/tests/SearchProduct.java
@@ -1,6 +1,7 @@
 package tests;
 
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -12,10 +13,10 @@ import java.io.IOException;
  * Assignment 3 - Automate 'Search Product' functionality of an e-commerce website
  * Test Case 1- Automate 'Search Product' Functionality of an e-commerce website with Selenium
  * Steps to Automate:
- * 1. Open link http://automationpractice.com/index.php - done
- * 2. Move your cursor over Women's link. - done
- * 3. Click on sub menu 'T-shirts' - done
- * 4. Get Name/Text of the first product displayed on the page. - done
+ * 1. Open link http://automationpractice.com/index.php
+ * 2. Move your cursor over Women's link.
+ * 3. Click on sub menu 'T-shirts'
+ * 4. Get Name/Text of the first product displayed on the page.
  * 5. Now enter the same product name in the search bar present on top of page and click search button.
  * 6. Validate that same product is displayed on searched page with same details which were displayed on T-Shirt's page.
  */
@@ -24,10 +25,12 @@ public class SearchProduct extends TestBase {
 
     BasePage basePage;
     Actions actions;
+    String firstProductName;
+    String firstSearchedProductName;
 
     @BeforeTest
     public void setUpTests() throws IOException {
-        logger.info("Setting up {} test-class", this.getClass().getSimpleName());
+        logger.info("Setting up test-class: {}", this.getClass().getSimpleName());
         driver = initializeDriver();
         // 1. Open link http://automationpractice.com/index.php
         driver.get(properties.getProperty("url"));
@@ -36,25 +39,47 @@ public class SearchProduct extends TestBase {
         actions = new Actions(driver);
     }
 
-    @Test
-    public void testCase1() {
+    @Test(priority = 0)
+    public void openWomenTshirtsPage() {
         // 2. Move your cursor over Women's link.
         // 3. Click on sub menu 'T-shirts'
         actions.moveToElement(basePage.getWomenLink()).moveToElement(basePage.getTshirtsLink()).click().build().perform();
+        Assert.assertTrue((driver.getTitle().toLowerCase()).contains("t-shirts"));
         logger.info("T-shirt page accessed");
+    }
+
+    @Test(priority = 1)
+    public void verifyThatMinOneProductIsDisplayedAndGetItsName() {
         // 4. Get Name/Text of the first product displayed on the page.
-        String firstProductName = basePage.getFirstProductNotHomepage().getText().trim();
+        firstProductName = basePage.getFirstProduct().getText().trim();
+        Assert.assertFalse(firstProductName.isEmpty());
         logger.info("Product list -> first product found: {}", firstProductName);
+    }
+
+    @Test(priority = 2)
+    public void searchTheProductViaSearchBar() {
         // 5. Now enter the same product name in the search bar present on top of page and click search button.
         actions.moveToElement(basePage.getSearchBox()).click().sendKeys(firstProductName).
                 moveToElement(basePage.getSearchButton()).click().build().perform();
+        Assert.assertTrue((driver.getTitle().toLowerCase()).contains("search"));
+        logger.info("Search results page accessed");
+    }
+
+    @Test(priority = 3)
+    public void verifyThatTheSameProductIsDisplayedOnSearchResults() {
+        // 6. Validate that same product is displayed on searched page with same details which were displayed on T-Shirt's page.
+        firstSearchedProductName = basePage.getFirstProduct().getText().trim();
+        Assert.assertFalse(firstProductName.isEmpty());
+        logger.info("Search results list -> first product found: {}", firstSearchedProductName);
+        Assert.assertEquals(firstSearchedProductName, firstProductName);
+        logger.info("The same product is displayed on Search results page with same name which was displayed on T-Shirt's page");
     }
 
     @AfterTest
     public void tearDownTests() {
-        logger.info("Tearing down {} test-class", this.getClass().getSimpleName());
-        /*if (driver != null) {
+        logger.info("Tearing down test-class: {}", this.getClass().getSimpleName());
+        if (driver != null) {
             driver.quit();
-        }*/
+        }
     }
 }
